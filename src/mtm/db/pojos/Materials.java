460c4b7e20f7abbf2b37ce74da12ea700b524edc
Alex <ale.pastrana@gmail.com>
2017-03-16 15:04:15 +0100

    Hi bitches

--- src/mtm/db/pojos/Materials.java
+++ src/mtm/db/pojos/Materials.java
@@ -1,5 +1,56 @@
 package mtm.db.pojos;
 
-public class Materials {
+import java.io.Serializable;
 
+public class Materials implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String type;
+	private Integer volume;
+	private Integer weight;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public Integer getVolume() {
+		return volume;
+	}
+	public void setVolume(Integer volume) {
+		this.volume = volume;
+	}
+	public Integer getWeight() {
+		return weight;
+	}
+	public void setWeight(Integer weight) {
+		this.weight = weight;
+	}
+	
+	
+	public Materials (){
+		super();
+	}
+	
+	public Materials(Integer id, String type, Integer weight, Integer volume){
+		super();
+		this.id= id;
+		this.type= type;
+		this.weight = weight;
+		this.volume = volume;
+	}
+	
+	
+	
+	
 }
