5027ed9bf9836354cf092dfc9deba0e3ad53c921
Alex <ale.pastrana@gmail.com>
2017-03-16 15:11:50 +0100

    Company

--- src/mtm/db/pojos/Materials.java
+++ src/mtm/db/pojos/Materials.java
@@ -51,6 +51,35 @@ public class Materials implements Serializable {
 	}
 	
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Materials other = (Materials) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Materials [id=" + id + ", type=" + type + ", volume=" + volume + ", weight=" + weight + "]";
+	}
+	
+	
 	
 	
 }
