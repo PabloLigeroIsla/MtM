99f7200bbb490476ca4d12d1507105024bf838f6
Alex <ale.pastrana@gmail.com>
2017-03-16 15:31:09 +0100

    lkjaskdlafkhjsdf

--- src/mtm/db/pojos/Company.java
+++ src/mtm/db/pojos/Company.java
@@ -1,9 +1,37 @@
 package mtm.db.pojos;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class Company implements Serializable {
 
 	private static final long serialVersionUID = 5399354394170770491L;
 
+	
+	private String resource;
+	private String location;
+	private String company_name;
+	
+	private List<Order>  materialList;
+	
+	public String getResource() {
+		return resource;
+	}
+	public void setResource(String resource) {
+		this.resource = resource;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getCompany_name() {
+		return company_name;
+	}
+	public void setCompany_name(String company_name) {
+		this.company_name = company_name;
+	}
+	
+	
 }
