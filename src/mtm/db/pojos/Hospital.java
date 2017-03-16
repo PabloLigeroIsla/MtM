08af4cfe17df007e22460d9e953b3a72bb89fba5
Pablo <Pablo@Lgs-PC>
2017-03-16 15:05:28 +0100

    Hospital finished

--- src/mtm/db/pojos/Hospital.java
+++ src/mtm/db/pojos/Hospital.java
@@ -16,7 +16,7 @@ public class Hospital implements Serializable
 	
 	private List<Order> orderList;
 	//Primary Key --> hospital_ID
-	//Foreign Key --> NO
+	//Foreign Key --> No
 	
 	//Gets and Sets
 	
