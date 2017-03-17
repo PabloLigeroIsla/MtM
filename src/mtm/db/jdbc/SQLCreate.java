0127bf1fe9a513c016132329ca14b0503ff526d6
Pablo <Pablo@Lgs-PC>
2017-03-17 13:11:41 +0100

    Update SQLCreate

--- src/mtm/db/jdbc/SQLCreate.java
+++ src/mtm/db/jdbc/SQLCreate.java
@@ -26,6 +26,24 @@ public class SQLCreate
 			
 			
 			//Pablo
+	        	// fPt -->firstPablotable //sqlp1 --> sqlpablo1
+			Statement fPt = c.createStatement();
+			String sqlp1 = "CREATE TABLE hospital("
+					+ "hospital_ID    INTEGER PRIMARY KEY AUTOINCREMENT,"
+					+ "name TEXT NOT NULL,"
+					+ "location TEXT NOT NULL,"
+					+ "medical_specialization TEXT NOT NULL)";
+			fPt.executeUpdate(sqlp1);
+			fPt.close();
+			
+			Statement sPt = c.createStatement();
+			String sqlp2 = "CREATE TABLE orders("
+					+ "order_ID INTEGER PRIMARY KEY AUTOINCREMENT,"
+					+ "total_amount_instruments INTEGER NOT NULL,"
+					+ "order_date DATE NOT NULL,"
+					+ "delivery_date DATE NOT NULL)";
+			sPt.executeUpdate(sqlp2);
+			sPt.close();
 			
 	//Close the Data Base Connection
 	c.close();
