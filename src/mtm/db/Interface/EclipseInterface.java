33e0684ce7bfb2fad23dadcd4951da5525ec3370
Pablo <Pablo@Lgs-PC>
2017-03-17 13:40:55 +0100

    More or Less structure of the menu

--- src/mtm/db/Interface/EclipseInterface.java
+++ src/mtm/db/Interface/EclipseInterface.java
@@ -1,6 +1,44 @@
 package mtm.db.Interface;
 
-public class EclipseInterface 
+
+ class EclipseInterface 
 {
+	public static void main()
+	{
+		
+	}
+
+
+
+	public int abrirMenu()
+	{
+		int opcion;
+		int numOpciones = 3; //Numero de opciones que podemos seleccionar con esta función
+		imprimirMenu();
+		opcion = selectNumber(numOpciones);
+		System.out.println("Selecciona la Opción que tu Quieras Wapi");
+		return opcion;
+	}
+
+	public void imprimirMenu()
+	{
+		//Si añades opciones, recuerda mirar elmétodo abrirMenu
+		System.out.println("Opcion 1.- Introduce Valores de Tabla\n"
+				+ "Opción 2.- Borrar Valores de la Tabla");
+	}
 	
+	public int selectNumber(int numOp)
+	{
+		int option = 0;
+		try
+		{
+			
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+			System.out.println("Error Introducing the values");
+			
+		}
+		return option;
+	}
 }
