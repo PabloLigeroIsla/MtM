b701bafecc089a86a8c722e33d71d17ca265a347
charofpb <charofpb@gmail.com>
2017-03-21 15:42:32 +0100

    asf

--- src/mtm/db/Interface/Validator.java
+++ src/mtm/db/Interface/Validator.java
@@ -11,7 +11,7 @@ public class Validator
 	//introducing the type of value we are storing in the variable.
 	
 	static BufferedReader c = new BufferedReader(new InputStreamReader(System.in));
-	
+	int b;
 	// Writing Methods
 	
 	public static int writeNumber() 
