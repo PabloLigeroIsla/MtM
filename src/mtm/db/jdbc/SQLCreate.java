ceee3b6170304a00bc02880a54f0c2f04695d643
Alex <ale.pastrana@gmail.com>
2017-03-23 15:06:17 +0100

    sqlcreate Alex aka handsome boy

--- src/mtm/db/jdbc/SQLCreate.java
+++ src/mtm/db/jdbc/SQLCreate.java
@@ -29,6 +29,27 @@ public class SQLCreate
 				+ "delivery_date DATE NOT NULL)";
 			sPt.executeUpdate(sqlp2);
 			sPt.close();
+			
+			//Alex
+			// fAt -->firstAlextable // sqla1 --> sqlalex1
+			Statement fAt = c.createStatement();
+			String sqla1 = "CREATE TABLE company ("
+					+ "resource TEXT PRIMARY KEY,"
+					+ "location TEXT,"
+					+"company_name TEXT);";
+			fAt.executeUpdate(sqla1);
+			fAt.close();
+			
+			Statement sAt = c.createStatement();
+			String sqla2 =  "CREATE TABLE materials ("
+							+"material_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+							+"weight INTEGER,"
+							+"volume INTEGER,"
+							+"material_provided  TEXT REFERENCES company(resource),"
+							+"machinery_type TEXT REFERENCES machinery(type));";
+			sAt.executeUpdate(sqla2);
+			sAt.close();
+			
 		}catch (Exception e)
 		{
 			e.printStackTrace();
