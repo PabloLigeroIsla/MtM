048bf8b4574a936811a2342193a351dcc83862f6
charofpb <charofpb@gmail.com>
2017-05-18 14:48:34 +0200

    chaaa

--- src/mtm/db/pojos/Instrument.java
+++ src/mtm/db/pojos/Instrument.java
@@ -14,6 +14,7 @@ import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 
 
+
 @Entity
 @Table(name = "instrument")
 @XmlAccessorType(XmlAccessType.FIELD) //Be able to use XML
