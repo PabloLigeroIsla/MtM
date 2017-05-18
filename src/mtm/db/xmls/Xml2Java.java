4aad99b1a2dcd29d656cfd16a0e8eaaa03ae7ec4
MARIA JESUS <MARIA JESUS@10.60.96.30>
2017-05-18 09:40:03 +0200

    XML

--- src/mtm/db/xmls/Xml2Java.java
+++ src/mtm/db/xmls/Xml2Java.java
@@ -32,6 +32,50 @@ public class Xml2Java
 		return hosp;
 	}
 	
+	public Employee employeeX2J(File file)
+	{
+		
+		Employee emp = null;
+		
+		try
+		{
+			JAXBContext jaxbContext = JAXBContext.newInstance(Employee.class);
+			Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+
+		emp = (Employee) unmarshaller.unmarshal(file);
+		
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		
+		
+		return emp;
+	}
+	
+	public Machinery machineryX2J(File file)
+	{
+		
+		Machinery mach = null;
+		
+		try
+		{
+			JAXBContext jaxbContext = JAXBContext.newInstance(Machinery.class);
+			Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+
+		mach = (Machinery) unmarshaller.unmarshal(file);
+		
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		
+		
+		return mach;
+	}
+	
 	public MtM mtmX2J(File file)
 	{
 		MtM mtm = null;
